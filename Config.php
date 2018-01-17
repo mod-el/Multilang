560675e6521d92128e8ecf9681232a0f48a6d368
Domenico Giambra <sviluppo@editt.it>
2018-01-17 12:28:41 +0100

    Languages management via ModEl control panel and several improvements

--- Config.php
+++ Config.php
@@ -4,8 +4,8 @@ use Model\Core\Module_Config;
 
 class Config extends Module_Config
 {
-	/** @var string */
-	protected $name = 'Multilang';
+	/** @var bool */
+	public $configurable = true;
 
 	/**
 	 * @param array $data
@@ -14,6 +14,17 @@ class Config extends Module_Config
 	public function install(array $data = []): bool
 	{
 		return (bool)file_put_contents(INCLUDE_PATH . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'Multilang' . DIRECTORY_SEPARATOR . 'dictionary.php', "<?php\n\$this->>dictionary = [];\n");
+	}
+
+	/**
+	 * Returns the config template
+	 *
+	 * @param array $request
+	 * @return string
+	 */
+	public function getTemplate(array $request)
+	{
+		return 'config';
 	}
 
 	/**
@@ -70,5 +81,78 @@ class Config extends Module_Config
 			]);
 		}
 		return true;
+	}
+
+	/**
+	 * Save the configuration
+	 *
+	 * @param string $type
+	 * @param array $data
+	 * @return bool
+	 * @throws \Model\Core\Exception
+	 */
+	public function saveConfig(string $type, array $data): bool
+	{
+		$config = $this->retrieveConfig();
+
+		$originalLanguages = $config['langs'];
+
+		$langs = explode(',', $data['langs']);
+		foreach ($langs as &$l) {
+			if (strlen($l) !== 2)
+				$this->model->error($l . ' is not a valid language');
+
+			$l = strtolower($l);
+		}
+		unset($l);
+
+		$defaultLang = $data['default'];
+		if (!in_array($defaultLang, $langs))
+			$this->model->error($defaultLang . ' is not among chosen languages');
+
+		$config['langs'] = $langs;
+		$config['default'] = $defaultLang;
+
+		if (!isset($config['tables']))
+			$config['tables'] = [];
+		if (!isset($config['type']))
+			$config['type'] = 'url';
+
+		$configFileDir = INCLUDE_PATH . 'app' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'Multilang';
+		if (!is_dir($configFileDir))
+			mkdir($configFileDir, 0777, true);
+
+		$w = file_put_contents($configFileDir . DIRECTORY_SEPARATOR . 'config.php', '<?php
+$config = ' . var_export($config, true) . ';
+');
+		if ($w) {
+			$this->model->_Multilang->reloadConfig();
+			if ($this->model->_Multilang->normalizeAllLangsInWords()) {
+				foreach ($this->model->_Multilang->langs as $l) {
+					if (!in_array($l, $originalLanguages)) {
+						foreach($this->model->_Multilang->tables as $t => $opt){
+							$fields = [];
+							$fields_prefixed = [];
+							foreach($opt['fields'] as $f){
+								$fields[] = '`'.$f.'`';
+								$fields_prefixed[] = 'l.`'.$f.'`';
+							}
+							$fields = implode(',', $fields);
+							$fields = $fields ? ','.$fields : '';
+							$fields_prefixed = implode(',', $fields_prefixed);
+							$fields_prefixed = $fields_prefixed ? ','.$fields_prefixed : '';
+
+							$this->model->_Db->query('INSERT INTO `'.$t.$opt['suffix'].'`(`'.$opt['keyfield'].'`,`'.$opt['lang'].'`'.$fields.') SELECT t.id,\''.$l.'\''.$fields_prefixed.' FROM `'.$t.'` t LEFT JOIN `'.$t.$opt['suffix'].'` l ON (l.`'.$opt['keyfield'].'` = t.id AND l.`'.$opt['lang'].'` = \''.$this->model->_Multilang->options['default'].'\') WHERE t.id NOT IN (SELECT l.`'.$opt['keyfield'].'` FROM `'.$t.$opt['suffix'].'` l WHERE l.`'.$opt['lang'].'` = \''.$l.'\')');
+						}
+					}
+				}
+
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 }
